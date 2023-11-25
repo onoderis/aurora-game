ae702389a6f707216d644dc4e35a60ebbf649f9a
onoderis <onoderis@users.noreply.github.com>
2023-11-25 10:59:48 +0600

    Display player box

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        // .add_system(input)
+        .run();
+}
+
+#[derive(Bundle)]
+struct PlayerBundle {
+    controllable: Controllable,
+    position: Position,
+    sprite_bundle: SpriteBundle,
+}
+
+/// Controllable entity by a player.
+#[derive(Component)]
+struct Controllable {}
+
+#[derive(Component)]
+struct Position { x: f32, y: f32 }
+
+fn setup(mut commands: Commands) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+
+    // Player
+    commands.spawn(
+        PlayerBundle {
+            controllable: Controllable {},
+            position: Position { x: 0., y: 0. },
+            sprite_bundle: SpriteBundle {
+                transform: Transform {
+                    translation: Vec3::new(0., 0., 10.),
+                    rotation: Quat::IDENTITY,
+                    scale: Vec3::new(100., 100., 0.),
+                },
+                sprite: Sprite {
+                    color: Color::BLUE,
+                    ..default()
+                },
+                ..default()
+            },
+        }
+    );
 }
